522d9d564573d7414ea285c74e5dacf32e9e0fb1
kcarnrite <kfcarnrite@gmail.com>
2021-05-01 17:49:13 +0000

    Login serving basic web form

--- webApp/login/urls.py
+++ webApp/login/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'login'
 urlpatterns = [
 
-path('', views.loginView, name='login'),
+path('', views.loginView, name='loginView'),
 ]
 
