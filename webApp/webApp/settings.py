a60b53972270055e6edcf709f4a983703dd3ed94
Kyle Carnrite <kfcarnrite@gmail.com>
2021-05-01 16:58:34 -0400

    Added model for Student

--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -121,4 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+AUTH_USER_MODEL = 'login.Student'
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
