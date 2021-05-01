7535d5f278979a8baefa2216327bd3939c051753
Kyle Carnrite <kfcarnrite@gmail.com>
2021-05-01 13:36:27 -0400

    Login not working

--- webApp/login/views.py
+++ webApp/login/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def loginView(request):
+    '''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            request.session['failed'] = False
+            return redirect('home:home_view')
+        else:
+            request.session['failed'] = True
+
+
+    failed = request.session.get('failed', False)
+    '''
+    failed = False
+    form = AuthenticationForm(request.POST)
+    context = { 'login_form' : form,
+                'failed' : failed}
+
+    return render(request, 'login.html', context)
