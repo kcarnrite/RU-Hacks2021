98bd6ecbc246273920aa5b096216bc779cc50d5f
jtan334 <60947320+jtan334@users.noreply.github.com>
2021-05-01 22:33:25 -0400

    trying to fix button

--- Front End/helpfulVideosScript.js
+++ Front End/helpfulVideosScript.js
@@ -2,6 +2,9 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
 var likes=0;
+var likes2=0;
+var isCounted= false;
+var isCounted2 = false;
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -28,14 +31,35 @@ var obj1 = {"video": {
     });
   }
 
-  for (const btn of document.querySelectorAll('.vote')) {
-    btn.addEventListener('click', event => {
-      likes +=1;
-      document.getElementById('numberOfLikes2').innerHTML +=likes;
-      document.getElementById('numberOfLikes').innerHTML +=likes;
-      
+  document.getElementById("btn").addEventListener("click", function() {
+    if (isCounted==false){
+        likes+=1;
+        isCounted=!isCounted;
+        document.getElementById('numberOfLikes').innerHTML =likes;
+    }  
+   
+    if (isCounted!=false){
+        likes-=1;
+        isCounted=!isCounted;
+        document.getElementById('numberOfLikes').innerHTML =likes;
+    }
+  
+});
+
+    document.getElementById("btn2").addEventListener("click", function() {
+        if (isCounted2!=true){
+            likes2+=1;
+        isCounted2=!isCounted2;
+        document.getElementById('numberOfLikes').innerHTML =likes2;
+        }  
+        
+        if (isCounted2=true) {
+        likes2-=1;
+        isCounted2=!isCounted2;
+        document.getElementById('numberOfLikes').innerHTML =likes2;
+        } 
+        
     });
-  }
 
   document.getElementById('obj').innerHTML += obj1.video.value;
   document.getElementById('description').innerHTML += "This is totes helpful!";
@@ -47,7 +71,7 @@ var obj1 = {"video": {
   document.getElementById('description2').innerHTML += "This Khan Academy Video Saved My Life No Cap";
   document.getElementById('title2').innerHTML +="HTML for Dummies";
   
-  document.getElementById('numberOfLikes2').innerHTML =likes;
+  document.getElementById('numberOfLikes2').innerHTML =likes2;
   
   
   
